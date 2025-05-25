a8601e74f9fe9706803130c1dd8db5dec1bc5fc1
Dev-Mzu <mzuveliletmboxo.com>
2025-05-25 18:32:20 +0200

    working firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,14 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./freelancerio-1be2f-firebase-adminsdk-fbsvc-9e2f8fc0de.json");
+//var serviceAccount = require("./freelancerio-1be2f-firebase-adminsdk-fbsvc-9e2f8fc0de.json");
+let serviceAccount;
+
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+  serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+}
+else{
+  serviceAccount = require("./freelancerio-1be2f-firebase-adminsdk-fbsvc-9e2f8fc0de.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
